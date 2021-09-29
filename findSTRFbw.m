399046f26d06bf8e6c15cd7e523ffdadff10aa44
kushalbakshi <kushalbakshi@tamu.edu>
2021-09-29 11:12:44 -0500

    Debugged for improved accuracy

--- findSTRFbw.m
+++ findSTRFbw.m
@@ -6,13 +6,12 @@ c = unique(c);
 c_dif = find(diff([1;c;1])~=1);
 [lmax, jmax] = max(diff(c_dif));
 start_max = c(jmax);
-e_temporal = range(taxis(start_max:lmax));
+e_temporal = range(taxis(start_max:c(lmax)));
 
 r = unique(r);
 r_dif = find(diff([1;r;1])~=1);
 [lmax, jmax] = max(diff(r_dif));
-end_max = r(jmax);
-e_spectral = range(faxis(1, lmax:end_max)/1000);
+e_spectral = range(faxis(1, r(jmax):r(lmax))/1000);
 
 inhibitory = STA < 0;
 [r, c] = find(inhibitory == 1);
@@ -20,10 +19,9 @@ c = unique(c);
 c_dif = find(diff([1;c;1])~=1);
 [lmax, jmax] = max(diff(c_dif));
 start_max = c(jmax);
-i_temporal = range(taxis(start_max:lmax));
+i_temporal = range(taxis(start_max:c(lmax)));
 
 r = unique(r);
 r_dif = find(diff([1;r;1])~=1);
 [lmax, jmax] = max(diff(r_dif));
-end_max = r(jmax);
-i_spectral = range(faxis(1, lmax:end_max)/1000);
+i_spectral = range(faxis(1, r(jmax):r(lmax))/1000);
